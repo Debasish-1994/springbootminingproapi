df509edc9b2d7d324ff150f7d1523a200784e038
Debasish <debasishpradhan890@gmail.com>
2020-08-13 00:49:01 +0530

    Spring Boot Initial Setup

---
+++ admincontrolsystem/src/main/java/com/dev/magma/admincontrolsystem/AdmincontrolsystemApplication.java
@@ -0,0 +1,13 @@
+package com.dev.magma.admincontrolsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmincontrolsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmincontrolsystemApplication.class, args);
+	}
+
+}
---
+++ admincontrolsystem/src/test/java/com/dev/magma/admincontrolsystem/AdmincontrolsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.magma.admincontrolsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmincontrolsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
