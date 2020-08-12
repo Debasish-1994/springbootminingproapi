3a32e04c666ddc3a2051540f05500bdb8be94290
Debasish <debasishpradhan890@gmail.com>
2020-08-13 01:50:42 +0530

    AuthenticateResponse class added dummyLogic in AuthenticateResponse added

--- admincontrolsystem/src/main/java/com/dev/magma/admincontrolsystem/controller/LoginController.java
+++ admincontrolsystem/src/main/java/com/dev/magma/admincontrolsystem/controller/LoginController.java
@@ -6,13 +6,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dev.magma.admincontrolsystem.model.AuthenticateResponse;
 import com.dev.magma.admincontrolsystem.model.AuthenticationRequest;
 
 @RestController
 public class LoginController {
+	AuthenticateResponse authenticateResponse;
+
 	@RequestMapping(value = "/authenticate", method = RequestMethod.POST)
-	public ResponseEntity<?> authenticateLogin(@RequestBody AuthenticationRequest authenticationRequest){
+	public ResponseEntity<?> authenticateLogin(@RequestBody AuthenticationRequest authenticationRequest) {
 		System.out.println(authenticationRequest.toString());
-		return ResponseEntity.ok(authenticationRequest.toString());
+		authenticateResponse = new AuthenticateResponse(true, "userAuthenticated", "Debasish0057", "somejwttoken");
+
+		return ResponseEntity.ok(authenticateResponse);
 	}
 }
---
+++ admincontrolsystem/src/main/java/com/dev/magma/admincontrolsystem/model/AuthenticateResponse.java
@@ -0,0 +1,99 @@
+package com.dev.magma.admincontrolsystem.model;
+
+import java.io.Serializable;
+
+public class AuthenticateResponse implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private boolean status;
+	private String desc, userName, token;
+
+	public AuthenticateResponse(boolean status, String desc, String userName, String token) {
+		super();
+		this.status = status;
+		this.desc = desc;
+		this.userName = userName;
+		this.token = token;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((desc == null) ? 0 : desc.hashCode());
+		result = prime * result + (status ? 1231 : 1237);
+		result = prime * result + ((token == null) ? 0 : token.hashCode());
+		result = prime * result + ((userName == null) ? 0 : userName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AuthenticateResponse other = (AuthenticateResponse) obj;
+		if (desc == null) {
+			if (other.desc != null)
+				return false;
+		} else if (!desc.equals(other.desc))
+			return false;
+		if (status != other.status)
+			return false;
+		if (token == null) {
+			if (other.token != null)
+				return false;
+		} else if (!token.equals(other.token))
+			return false;
+		if (userName == null) {
+			if (other.userName != null)
+				return false;
+		} else if (!userName.equals(other.userName))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "AuthenticateResponse [status=" + status + ", desc=" + desc + ", userName=" + userName + ", token="
+				+ token + "]";
+	}
+
+}
